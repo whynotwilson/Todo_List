3fb6f61fb331c5a69611ecc96db8846f0367a66d
Wilson Tseng <p780729@hotmail.com>
2020-03-03 16:04:27 +0800

    Add user registration feature

--- routes/user.js
+++ routes/user.js
@@ -19,7 +19,33 @@ router.get('/register', (req, res) => {
 
 // 註冊檢查
 router.post('/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, password2 } = req.body
+  User.findOne({ email: email }).then(user => {
+    // 檢查要註冊的 email 是否已存在
+    if (user) {
+      // 已存在返回註冊頁面
+      console.log('User already exists')
+      res.render('register', {
+        name,
+        email,
+        password,
+        password2
+      })
+    } else {
+      // 不存在則新增使用者，新增完成後導回首頁
+      const newUser = new User({
+        name,
+        email,
+        password
+      })
+      newUser
+        .save()
+        .then(user => {
+          res.redirect('/')
+        })
+        .catch(err => console.log(err))
+    }
+  })
 })
 
 // 登出
