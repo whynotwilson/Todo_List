08c34bf134a8c50fdb6c2f5e366c2e389f9770a2
Wilson <p780729@hotmail.com>
2020-03-05 23:51:07 +0800

    Add bcryptjs to login route

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = passport => {
   passport.use(
@@ -11,10 +12,15 @@ module.exports = passport => {
         if (!user) {
           return done(null, false, { message: 'That email is not registered' })
         }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password incorrect' })
-        }
-        return done(null, user)
+        // 用 bcryptjs 比較使用者輸入的密碼及資料庫的密碼是否為同一組字串
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err
+          if (isMatch) {
+            return done(null, user)
+          } else {
+            return done(null, false, { message: 'Email or Password incorrect' })
+          }
+        })
       })
     })
   )
